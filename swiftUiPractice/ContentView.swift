fec1603d9e6c0879bec42c9cedf5ec6faf701aa3
dilipofficial <dilipofficial11@gmail.com>
2024-08-10 21:43:38 +0530

    background linear gradient

--- swiftUiPractice/ContentView.swift
+++ swiftUiPractice/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+//            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .leading, endPoint: .bottom)
+//                .edgesIgnoringSafeArea(.all)
         }
-        .padding()
     }
 }
 
