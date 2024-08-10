da54ab3d80e3ecfebe9972723858b9d30b1f0a7e
dilipofficial <dilipofficial11@gmail.com>
2024-08-10 21:01:54 +0530

    Initial Commit

---
+++ swiftUiPractice/swiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUiPracticeApp.swift
+//  swiftUiPractice
+//
+//  Created by Dilip Kumar on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
