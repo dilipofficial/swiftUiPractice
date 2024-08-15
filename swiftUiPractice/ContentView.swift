19b4e2d6904b935fbc376e8e9006c5eca21193d6
dilipkumar <dilipofficial11@gmail.com>
2024-08-15 18:14:58 +0530

    created cuperinto text

--- swiftUiPractice/ContentView.swift
+++ swiftUiPractice/ContentView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-//            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .leading, endPoint: .bottom)
-//                .edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .leading, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Cupertino, CA")
+                    .foregroundColor(.white)
+                    .font(.system(size: 32, weight: .medium))
+                    .padding()
+                Spacer()
+            }
         }
     }
 }
